a5fdfc9e9ef05a95ca6e05cd37bd64173ac62e46
Philipp Katzenberger <katzenberger.ph+github@gmail.com>
2023-11-07 20:00:43 +0100

    feat(main): Basic functionality

--- main.c
+++ main.c
@@ -2,6 +2,21 @@
 
 int main()
 {
-    printf("Hello World!\n");
+    unsigned int milk = 0, sugar = 0;
+    double price;
+
+    if(milk== 0 && sugar == 0){
+        printf("Kaffee so schwarz wie mein Humor\n");
+    } else if(milk != 0 && sugar != 0){
+        printf("Kaffee mit Milch und Zucker\n");
+    } else if(milk == 0 && sugar == 1){
+        printf("Kaffee mit Zucker ohne Milch\n");
+    } else if(milk == 1 && sugar == 0){
+        printf("Kaffee mit Milch ohne Zucker\n");
+    } else {
+        printf("Ausnahmefehler! Informieren Sie den Kundendienst!\n");
+    }
+
+    printf("\n\n");
     return 0;
 }
